7015445c88275efb1aafc21193a9956e4f72d192
HCKME <k.sarvesh1997@gmail.com>
2022-02-15 03:58:45 +0530

    0

--- js/diary.js
+++ js/diary.js
@@ -1,8 +1,515 @@
-let diaryContainerCaller = () => { diaryContainer(); }
+let diaryContainerCaller = () => {
+    diaryContainer();
+    diarySt1ClassEventListener();
+}
 
 let diaryContainer = () => {
     var diarySt1 = document.createElement('div');
     diarySt1.className = 'diarySt1Class';
-    diarySt1.innerHTML = `... Wanna read me?`;
+    diarySt1.innerHTML = `-_-`;
     document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+
+    var diarySt1 = document.createElement('div');
+    diarySt1.className = 'diarySt1Class';
+    diarySt1.innerHTML = `-_-`;
+    document.querySelector('#postId3').appendChild(diarySt1);
+}
+
+let diarySt1ClassEventListener = () => {
+    const boxes = [...document.querySelectorAll(".diarySt1Class")];
+    for (box of boxes) {
+        box.addEventListener('click', event => {
+            document.querySelector(".diarySt1Class").classList.add("appendDiary");
+        })
+    }
 }
